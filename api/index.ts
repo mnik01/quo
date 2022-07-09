f4fb20e332dc77f9e6e8d07670a5df1e56e898c8
mnik01 <maksimgrinberg01@gmail.com>
2022-07-09 16:46:45 +0600

    feat: axios added

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,17 @@
-export const getTodos = async () => ['1', '2', '3']
-export const postTodo = async (todo) => {return};
+// import axios from "axios";
+
+export const getTodos = async () => {
+  try {
+    // const response = await axios.get('/todos');
+    return ['1', '2', '3']
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const postTodo = async (todo) => {
+
+  // await axios.post('/todo', todo')
+
+  return;
+};
