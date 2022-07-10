5e18b327041a7f6e2ba82ccfb3bf7a587acbfda0
mnik01 <maksimgrinberg01@gmail.com>
2022-07-10 23:13:51 +0600

    feat: imgs updated

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider, onlineManager } from 'react-query'
 import { TailwindProvider } from 'tailwindcss-react-native';
+import NetInfo from '@react-native-community/netinfo'
 import { View, Text } from './components/Natives';
 import { Fetcher } from './components/Fetcher';
 import { StatusBar } from 'expo-status-bar';
 import { VFC } from 'react';
+
+onlineManager.setEventListener(setOnline =>
+  NetInfo.addEventListener(state => {
+    setOnline(state.isConnected)
+  })
+)
 
 const queryClient = new QueryClient()
 
@@ -13,7 +20,7 @@ const App: VFC = () => (
       <View className="flex-1 items-center justify-center bg-red-300">
         <Text className="text-white text-3xl">Quo Vadis?</Text>
         <Fetcher />
-        <StatusBar style="auto" />
+        <StatusBar style="dark" />
       </View>
     </TailwindProvider>
   </QueryClientProvider>
